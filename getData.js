edb9bc7bf317dba3bafdb97251a35b6f1aba40e7
Nick <n.armour0@gmail.com>
2015-12-14 22:44:39 -0800

    Initial Files

---
+++ getData.js
@@ -0,0 +1,28 @@
+function makeSummoner(){
+    var newSummoner = new Summoner();
+     getID(newSummoner);
+}
+
+function Summoner(){
+    this.sName =$("#sName").val();
+    this.sID = 0;
+
+}
+
+
+function getID (player){
+    
+    $.ajax({
+        url:'https://na.api.pvp.net/api/lol/na/v1.4/summoner/by-name/'+ player.sName+'?api_key=03c8f9d3-d619-4593-ad25-6bdfe94517c1',
+        type:'GET',
+        dataType:'json',
+        data:{},
+        success:function(json){
+             
+             player.sID = json[player.sName].id;
+             document.getElementById("number").innerHTML = player.sID;
+            
+        }
+    });
+}
+
