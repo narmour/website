242fa9e6ec7fd8b5c3b4f04fffa135db4bc409f1
Nick <n.armour0@gmail.com>
2015-12-16 17:44:33 -0800

    Updated ID names, about to implement Most played champs

--- js/getData.js
+++ js/getData.js
@@ -5,35 +5,42 @@
 $("document").ready(function() {
     $("input[type = 'text']#sName").val("GooocheyMang");
 
-    //when submit button is pushed, get the summonerID
+
+    //make the list of stats look nice
+    $("#summonerInfo").menu({
+        position: {
+            my: "center top",
+            at:"center bottom"
+        }
+    });
+
+
+    //when submit button is pushed, get summoner data.
     $('#submitbutton').click(function(){
-        $('#stats').empty();
+        //get basic summonerInfo    ID and Level
+        $('#summonerInfo').empty();
         var name = $('#sName').val();
-        var $statList = $('#stats');
+        var $summonerInfo = $('#summonerInfo');
          $.ajax({
                 type: 'GET',
                 url: 'https://na.api.pvp.net/api/lol/na/v1.4/summoner/by-name/'+name+'?api_key=03c8f9d3-d619-4593-ad25-6bdfe94517c1',
                 success: function(data){
                     console.log(data);
                     $.each(data, function(i, item){
-                        $statList.append('<li>ID: '+item.id+'</li>');
-                        $statList.append('<li>Name: '+item.name+'</li>');
-                        $statList.append('<li>Level: '+item.summonerLevel+'</li>');
+                        $summonerInfo.append('<li>ID: '+item.id+'</li>');
+                        $summonerInfo.append('<li>Name: '+item.name+'</li>');
+                        $summonerInfo.append('<li>Level: '+item.summonerLevel+'</li>');
                     });
                 },
                 error: function(){
                     $statList.append('<li>Please enter a summoner name</li>');
                 }
         });
+         
+
     });
 
-    //make the list of stats look nice
-    $("#stats").menu({
-        position: {
-            my: "center top",
-            at:"center bottom"
-        }
-    });
+    
 
 });
 
